f745565f9f95de6d77990bdc5844a71c0c169837
Walid Mostafa <amadalioo980@gmail.com>
2026-01-26 17:48:30 +0200

    last meeting update 26/1/2026 -2

--- src/pages/Chat/sections/ChatHeader.jsx
+++ src/pages/Chat/sections/ChatHeader.jsx
@@ -1,17 +1,21 @@
 import { useTranslation } from "react-i18next";
 import logo from "@/assets/images/logo.png";
+import { useSelector } from "react-redux";
 
 const ChatHeader = () => {
   const { t } = useTranslation();
+  const { setting } = useSelector((state) => state.setting);
 
   return (
-    <div className="py-3 border-b text-white flex items-center gap-2">
-      <img
-        loading="lazy"
-        src={logo}
-        alt="logo"
-        className="w-18 border-e-2 border-white pe-2"
-      />
+    <div className="pb-3 border-b text-white flex items-center gap-2">
+      <div className="w-20 h-12 overflow-hidden border-e-2 border-white pe-2">
+        <img
+          loading="lazy"
+          src={setting?.header_logo || logo}
+          alt="logo"
+          className="w-full h-full object-contain"
+        />
+      </div>
       <p className="font-bold text-xl">{t("chatHeader.admin")}</p>
     </div>
   );
